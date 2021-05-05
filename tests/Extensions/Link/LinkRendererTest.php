eecd4e71848ba8af736e34643e3153f28e1b6a27
Brian Faust <hello@basecode.sh>
2021-05-05 12:53:44 +0300

    wip

---
+++ tests/Extensions/Link/LinkRendererTest.php
@@ -0,0 +1,17 @@
+<?php
+
+it('should render an external link', function () {
+    //
+});
+
+it('should render an internal link if it contains the host', function () {
+    //
+});
+
+it('should render an internal link if it contains an anchor', function () {
+    //
+});
+
+it('should render an internal link if it is a relative path', function () {
+    //
+});
