6e958ead470ff31ba36f5a2016eb6422d8cdc5c1
Brian Faust <faustbrian@users.noreply.github.com>
2021-05-28 08:22:13 +0300

    fix: ensure consistent code output

--- src/Extensions/Highlighter/CodeBlockHighlighter.php
+++ src/Extensions/Highlighter/CodeBlockHighlighter.php
@@ -8,15 +8,16 @@ class CodeBlockHighlighter
 {
     public function highlight(string $codeBlock, ?string $language = null)
     {
-        if ($language === "xml") {
-            return $codeBlock;
+        if (str_contains($codeBlock, "<")) {
+            preg_match('#<\s*?code\b[^>]*>(.*?)</code\b[^>]*>#s', $codeBlock, $matches);
+
+            $codeBlock = $matches[1];
+        } else {
+            $codeBlock = trim(htmlspecialchars_decode(strip_tags($codeBlock)));
         }
 
         try {
-            return vsprintf('<code class="hljs-copy language-%s">%s</code>', [
-                $language,
-                trim(htmlspecialchars_decode(strip_tags($codeBlock))),
-            ]);
+            return vsprintf('<code class="hljs-copy language-%s">%s</code>', [$language, $codeBlock]);
         } catch (DomainException $e) {
             return $codeBlock;
         }
