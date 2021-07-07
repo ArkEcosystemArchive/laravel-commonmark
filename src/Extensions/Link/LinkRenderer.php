aa904107c3d39e98429e8789206c53aa24be8f6b
leMaur <leMaur@users.noreply.github.com>
2021-07-07 08:39:40 +0000

    style: resolve style guide violations

--- src/Extensions/Link/LinkRenderer.php
+++ src/Extensions/Link/LinkRenderer.php
@@ -3,7 +3,6 @@
 namespace ARKEcosystem\CommonMark\Extensions\Link;
 
 use Illuminate\Support\Arr;
-use Illuminate\Support\Str;
 use Illuminate\View\ComponentAttributeBag;
 use League\CommonMark\ElementRendererInterface;
 use League\CommonMark\HtmlElement;
